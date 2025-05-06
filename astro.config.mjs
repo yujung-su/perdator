1f5760cc5566fdd82498e93b8541f75867328973
yujung-su <raycc8899@gmail.com>
2025-05-06 23:10:38 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://yujung-su.github.io',
+  base: '/predator'
+});
